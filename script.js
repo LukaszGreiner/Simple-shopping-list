0dd368645959fed09dfd5a340f0f3062a34f0e67
Łukasz <devlukaszgreiner@gmail.com>
2024-03-18 12:19:00 +0100

    Revert "BLocked zooming, resized icon, dfoucs on modal inout"
    This reverts commit be0467468d9204763834bf716d2ae747a3499571.

--- script.js
+++ script.js
@@ -1,69 +1,152 @@
 const shareBtn = document.querySelector(".header__share-button");
 const shoppingList = document.querySelector(".shopping-list");
-const shoppingListItems = document.querySelectorAll(".shopping-list__item");
 const modal = document.querySelector(".modal");
 const optionsShareBtn = document.getElementById("modalShareBtn");
 const optionsCopyBtn = document.getElementById("modalCopyBtn");
 const optionsPasteBtn = document.getElementById("modalPasteBtn");
-const productNameInput = document.querySelector(".modal__input");
+const itemNameInput = document.querySelector(".modal__input");
 const openModalBtn = document.getElementById("openModalBtn");
 const submitModalBtn = document.getElementById("modalSubmitBtn");
 
-let productsData = [];
-let sms = "";
-submitModalBtn.addEventListener("click", addProduct);
-loadFromLocalStorage();
-printProducts(productsData);
+const itemTemplate = document.getElementById("item-template");
 
-// LOCAL STORAGE
+//LOCAL STORAGE
+let shoppingListData =
+  JSON.parse(localStorage.getItem("shoppingListData")) || [];
 
-// Load data from local storage
-function loadFromLocalStorage() {
-  if (localStorage.getItem("productsArray")) {
-    productsData = JSON.parse(localStorage.getItem("productsArray"));
-  }
+// Event handlers
+openModalBtn.addEventListener("click", openModal);
+submitModalBtn.addEventListener("click", handleSubmitModal);
+itemNameInput.addEventListener(
+  "keyup",
+  (e) => e.key === "Enter" && handleSubmitModal()
+);
+
+// MODAL
+// Open modal
+function openModal() {
+  modal.showModal();
+  setTimeout(() => itemNameInput.focus(), 100);
 }
 
-// Save data to local storage
-function saveToLocalStorage() {
-  productsData = Array.from(
-    shoppingList.querySelectorAll(".shopping-list__item")
-  ).map((product) => {
-    return product.querySelector(".shopping-list__item__name").textContent;
+function closeModal() {
+  modal.close();
+}
+
+function handleSubmitModal() {
+  // Filter user input
+  if (itemNameInput.value.trim() === "") {
+    closeModal();
+    return;
+  }
+  const filteredData = filterInput(itemNameInput.value); //  returns [{item1}, {item2}, {item3]
+
+  // Update shoppingListData
+  shoppingListData.push(...filteredData);
+  console.log(shoppingListData);
+
+  // Render shoppingList from shoppingListData
+  renderShoppingListData();
+  save();
+  // Clear input field
+  itemNameInput.value = "";
+}
+
+function createItemNode(itemObj) {
+  const itemNode = document.importNode(itemTemplate.content, true);
+  itemNode
+    .querySelector("li")
+    .setAttribute("data-id", itemObj.id ? itemObj.id : uid());
+  itemNode.querySelector("p").textContent = itemObj.name
+    ? itemObj.name
+    : "placeholder";
+  itemNode.querySelector("input").checked = itemObj.isChecked;
+
+  // Hook event handlers
+  // Removing item from the DOM and shoppingListData
+  itemNode.querySelector("button").addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+    save();
+  });
+  // Saving checkbox state in the shoppingListData
+  itemNode.querySelector("input").addEventListener("change", save);
+
+  return itemNode;
+}
+
+function renderShoppingListData() {
+  // Clear shopping list
+  shoppingList.innerHTML = "";
+  // Render items from shoppingListData
+  shoppingListData.map((item) => {
+    const itemNode = createItemNode(item);
+    shoppingList.appendChild(itemNode);
+  });
+}
+
+function save() {
+  shoppingListData = [];
+  const shoppingListItems = document.querySelectorAll(".shopping-list__item");
+
+  shoppingListItems.forEach((item) => {
+    shoppingListData.push({
+      id: item.dataset.id,
+      name: item.querySelector("p").textContent,
+      isChecked: item.querySelector("input").checked,
+    });
   });
 
-  localStorage.setItem("productsArray", JSON.stringify(productsData));
+  localStorage.setItem("shoppingListData", JSON.stringify(shoppingListData));
+  console.log("Saved: ", shoppingListData);
+}
+
+//Fiter user Input
+function filterInput(itemName) {
+  let filteredData = [];
+  if (itemName.includes(",")) {
+    filteredData = itemName
+      .split(",")
+      .map((item) => item.trim())
+      .filter((item) => item);
+  } else if (itemName.includes("-")) {
+    filteredData = itemName
+      .split("-")
+      .map((item) => item.trim())
+      .filter((item) => item);
+  } else if (itemName !== "") {
+    filteredData = [itemName.trim()];
+  }
+
+  // [item1, item2] -> [{"name":"item1"},{name:"item2"}]
+  filteredData = filteredData.map((itemName) => {
+    return { name: itemName };
+  });
+
+  return filteredData;
 }
 
 // MODAL MENU OPTIONS
-
 function createSMS() {
-  let result = "";
-  productsData.forEach((product) => {
-    result += `-${product}\n`;
-  });
-
-  sms = `
-  Lista zakupów:
-  ${"\n- " + productsData.toString().replace(/,/g, "\n- ")}
+  const itemNames = shoppingListData.map((item) => item.name).join("\n- ");
+  const sms = `Lista zakupów:
+  ${"\n- " + itemNames}
   \nSkopiuj wiadomość i kliknij link, aby wyświetlić listę zakupów w aplikacji.
-🔗 https://prostalistazakupow.netlify.app/`;
+  🔗 https://prostalistazakupow.netlify.app/`;
+
+  return sms;
 }
 
 // Share list via socials
 shareBtn.addEventListener("click", () => {
-  if (productsData == []) {
-    console.log(productsData);
-    alert("Dodaj produkty");
-    return;
-  }
-  createSMS();
+  if (shoppingListData == "") return alert("Dodaj produkty");
+
   if (navigator.share) {
     navigator.share({
-      text: sms,
+      text: createSMS(),
     });
   } else {
-    navigator.clipboard.writeText(sms);
+    navigator.clipboard.writeText(sms) &&
+      alert("Skopiowano listę zakupów do schowka");
   }
 });
 optionsShareBtn.addEventListener("click", () => {
@@ -72,92 +155,55 @@ optionsShareBtn.addEventListener("click", () => {
 
 // Copy list to the clipboard
 optionsCopyBtn.addEventListener("click", async () => {
-  if (productsData == "") alert("Dodaj produkty");
-  else {
-    createSMS();
-    await navigator.clipboard.writeText(sms);
-    alert(`Skopiowano do schowka listę produktów!`);
+  if (shoppingListData == "") return alert("Dodaj produkty");
+
+  try {
+    await navigator.clipboard.writeText(createSMS());
+    alert(`Skopiowano listę zakupów do schowka`);
+  } catch (error) {
+    console.error(error.message);
+    alert("Nie udało się wczytać listy zakupów ze schowka\n\n" + error.message);
   }
 });
 
 //Paste list from the clipboard
 optionsPasteBtn.addEventListener("click", async () => {
-  const read = await navigator.clipboard.readText();
-  productNameInput.value = read.slice(19, -122);
+  try {
+    const read = await navigator.clipboard.readText();
+    if (read.includes("https://prostalistazakupow.netlify.app/")) {
+      itemNameInput.value = read.slice(19, read.lastIndexOf("S"));
+    }
+  } catch (error) {
+    console.error(error.message);
+    alert("Nie udało się wczytać listy zakupów ze schowka\n\n" + error.message);
+  }
+
   submitModalBtn.focus();
 });
 
 // Paste directly into input field
-productNameInput.addEventListener("paste", (e) => {
-  e.preventDefault();
-  const pastedData = e.clipboardData.getData("text/plain");
-  const modifiedData = pastedData.slice(19, -122);
+itemNameInput.addEventListener("paste", (event) => {
+  event.preventDefault();
+  let modifiedData = "";
+  let pastedData = "";
 
-  productNameInput.value = modifiedData;
-});
-
-// Open modal
-openModalBtn.addEventListener("click", (e) => {
-  modal.showModal();
-  setTimeout(() => productNameInput.focus(), 100);
-});
-
-// Add product
-function addProduct() {
-  let productName = productNameInput.value;
-  let productsArray = [];
-
-  // Filter sms, when paste EventListnerFails
-  productNameInput.value.includes("https://prostalistazakupow.netlify.app/")
-    ? (productName = productNameInput.value.slice(19, -122))
-    : null;
-
-  // Input filtering
-  if (productName.includes(",")) {
-    productsArray = productName
-      .split(",")
-      .map((item) => item.trim())
-      .filter((item) => item);
-  } else if (productName.includes("-")) {
-    productsArray = productName
-      .split("-")
-      .map((item) => item.trim())
-      .filter((item) => item);
-  } else if (productName.trim() != "") {
-    productsArray = [productName];
+  try {
+    pastedData = event.clipboardData.getData("text/plain");
+    if (pastedData.includes("https://prostalistazakupow.netlify.app/")) {
+      modifiedData = pastedData.slice(19, pastedData.lastIndexOf("S"));
+    }
+  } catch (error) {
+    console.error(error.message);
+    alert("Nie udało się wkleić listy zakupów do schowka");
   }
 
-  printProducts(productsArray);
-  saveToLocalStorage();
+  modifiedData
+    ? (itemNameInput.value = modifiedData)
+    : (itemNameInput.value = pastedData);
+});
 
-  // Clear input and close modal
-  productsArray = [];
-  productNameInput.value = "";
-  modal.close();
-}
+const uid = function () {
+  return Math.random().toString(36).slice(2, -1);
+};
 
-// Load products
-function printProducts(productsArray) {
-  // Create shopping list items
-  productsArray.forEach((item) => {
-    const productListItem = `
-      <li class="shopping-list__item">
-        <input type="checkbox" class="shopping-list__item__checkbox" />
-        <p class="shopping-list__item__name">${item}</p>
-        <button type="button" class="shopping-list__item__remove-button">X</button>
-      </li>
-    `;
-
-    // Insert product list item into DOM
-    shoppingList.insertAdjacentHTML("beforeend", productListItem);
-
-    // Hook event listener to remove button
-    shoppingList.lastElementChild
-      .querySelector(".shopping-list__item__remove-button")
-      .addEventListener("click", (event) => {
-        const item = event.target.parentElement;
-        item.remove();
-        saveToLocalStorage();
-      });
-  });
-}
+renderShoppingListData();
